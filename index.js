10e81e536efdde044481959dc24b60837b989cd9
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-26 22:36:19 +0600

    images changed

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const allCourses = require("./data/courses.json");
 
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
 
+app.get("/allCourses", (req, res) => {
+  res.send(allCourses);
+});
 // app.get('/categories/:id', (req, res) => {
 //   const id
 // })
